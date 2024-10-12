1790ccc37a61ffd230405fa5117d034ca2d6f2c9
Yasmin123-design <https://github.com/Yasmin123-design/BMICalculator.git>
2024-10-12 05:37:06 +0300

    initialcommit

---
+++ BMICalculator/Controllers/BMIController.cs
@@ -0,0 +1,33 @@
+﻿using BMICalculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace BMICalculator.Controllers
+{
+	public class BMIController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult CaculateBMI(BMIModel bMI)
+		{
+			if (ModelState.IsValid)
+			{
+
+				bMI.BMI = bMI.Weight / (bMI.Height * bMI.Height);
+				if (bMI.BMI < 18.5)
+					ViewBag.Result = "نقص وزن";
+				else if (bMI.BMI >= 18.5 && bMI.BMI < 24.9)
+					ViewBag.Result = "وزن طبيعى";
+				else if (bMI.BMI >= 25 && bMI.BMI < 29.9)
+					ViewBag.Result = "زيادة الوزن";
+				else
+					ViewBag.Result = "سمنة";
+				return View("Result",bMI);
+			}
+			return View("Index");
+		}
+	}
+}
---
+++ BMICalculator/Models/BMIModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BMICalculator.Models
+{
+	public class BMIModel
+	{
+		public float Weight { get; set; }
+		public float Height { get; set; }
+		public float BMI { get; set; }
+	}
+}
